7e968ac288c8eb6c8e74fa4cc4bba633a7ab6492
wuddat <steven.a.briscoe@gmail.com>
2025-06-17 21:06:25 -0400

    multihit bugfix

--- data/moves/attack.gd
+++ data/moves/attack.gd
@@ -67,67 +67,69 @@ func apply_effects(targets: Array[Node], modifiers: ModifierHandler, battle_unit
 						break
 
 	#Primary Hit
-	var damage_effect := DamageEffect.new()
-	var type_multiplier := Effectiveness.get_multiplier(damage_type, primary_target.stats.type)
-	print("Damage type is: %s against target: %s for multiplyer of %s" % [damage_type, primary_target.stats.type, type_multiplier])
-	var effectiveness_damage = modifiers.get_modified_value(final_damage, Modifier.Type.DMG_DEALT)
-	damage_effect.amount = round(effectiveness_damage * type_multiplier)
-	if type_multiplier > 1:
-		damage_effect.sound = preload("res://art/sounds/sfx/supereffective.wav")
-	elif type_multiplier < 1:
-		damage_effect.sound = preload("res://art/sounds/not_effective.wav")
-	else:
-		damage_effect.sound = sound
-	total_damage_dealt += damage_effect.amount
-	if targets.size() < 2:
-		damage_effect.execute([primary_target])
-	else: damage_effect.execute(targets)
+	if is_instance_valid(primary_target):
+		var damage_effect := DamageEffect.new()
+		var type_multiplier := Effectiveness.get_multiplier(damage_type, primary_target.stats.type)
+		print("Damage type is: %s against target: %s for multiplyer of %s" % [damage_type, primary_target.stats.type, type_multiplier])
+		var effectiveness_damage = modifiers.get_modified_value(final_damage, Modifier.Type.DMG_DEALT)
+		damage_effect.amount = round(effectiveness_damage * type_multiplier)
+		if type_multiplier > 1:
+			damage_effect.sound = preload("res://art/sounds/sfx/supereffective.wav")
+		elif type_multiplier < 1:
+			damage_effect.sound = preload("res://art/sounds/not_effective.wav")
+		else:
+			damage_effect.sound = sound
+		total_damage_dealt += damage_effect.amount
+		if targets.size() < 2:
+			damage_effect.execute([primary_target])
+		else: damage_effect.execute(targets)
+			
 		
-	
-	#Splash Hit
-	for splash_target in splash_targets:
-		var splash_effect := DamageEffect.new()
-		splash_effect.amount = modifiers.get_modified_value(final_splash, Modifier.Type.DMG_DEALT)
-		splash_effect.execute([splash_target])
-		total_damage_dealt += splash_effect.amount
-		print("the execute has been executed for splash")
-	
-	if battle_unit_owner != null:
-		if battle_unit_owner.status_handler.has_status("critical"):
-			battle_unit_owner.status_handler.has_and_consume_status("critical")
-			var dmg_dealt_mod := battle_unit_owner.modifier_handler.get_modifier(Modifier.Type.DMG_DEALT)
-			if dmg_dealt_mod:
-				dmg_dealt_mod.remove_value("critical")
-				print("critical consumed")
+		#Splash Hit
+		for splash_target in splash_targets:
+			var splash_effect := DamageEffect.new()
+			splash_effect.amount = modifiers.get_modified_value(final_splash, Modifier.Type.DMG_DEALT)
+			splash_effect.execute([splash_target])
+			total_damage_dealt += splash_effect.amount
+			print("the execute has been executed for splash")
+		
+		if battle_unit_owner != null:
+			if battle_unit_owner.status_handler.has_status("critical"):
+				battle_unit_owner.status_handler.has_and_consume_status("critical")
+				var dmg_dealt_mod := battle_unit_owner.modifier_handler.get_modifier(Modifier.Type.DMG_DEALT)
+				if dmg_dealt_mod:
+					dmg_dealt_mod.remove_value("critical")
+					print("critical consumed")
 
-	
-	#apply status effect if any to ALL ENEMIES
-	for status_effect in status_effects:
-		if status_effect:
-			var stat_effect := StatusEffect.new()
-			var status_to_apply := status_effect.duplicate()
-			stat_effect.status = status_to_apply
-			stat_effect.execute(targets)
 		
-	#user recoil damage if any
-	if self_damage > 0:
-		var self_dmg_effect := DamageEffect.new()
-		self_dmg_effect.amount = self_damage
-		self_dmg_effect.sound = null
-		self_dmg_effect.execute([battle_unit_owner])
-		total_damage_dealt += self_dmg_effect.amount
-	
-	if self_heal > 0:
-		var self_heal_effect := HealEffect.new()
-		self_heal_effect.amount = total_damage_dealt/2
-		#TODO add heal effect sfx
-		self_heal_effect.sound = null
-		self_heal_effect.execute([battle_unit_owner])
+		#apply status effect if any to ALL ENEMIES
+		for status_effect in status_effects:
+			if status_effect:
+				var stat_effect := StatusEffect.new()
+				var status_to_apply := status_effect.duplicate()
+				stat_effect.status = status_to_apply
+				stat_effect.execute(targets)
+			
+		#user recoil damage if any
+		if self_damage > 0:
+			var self_dmg_effect := DamageEffect.new()
+			self_dmg_effect.amount = self_damage
+			self_dmg_effect.sound = null
+			self_dmg_effect.execute([battle_unit_owner])
+			total_damage_dealt += self_dmg_effect.amount
 		
-	#status effects on user if any
-	for self_stat in self_status:
-		if self_stat:
-			var self_effect := StatusEffect.new()
-			var status_to_apply := self_stat.duplicate()
-			self_effect.status = status_to_apply
-			self_effect.execute([battle_unit_owner])
+		if self_heal > 0:
+			var self_heal_effect := HealEffect.new()
+			self_heal_effect.amount = total_damage_dealt/2
+			#TODO add heal effect sfx
+			self_heal_effect.sound = null
+			self_heal_effect.execute([battle_unit_owner])
+			
+		#status effects on user if any
+		for self_stat in self_status:
+			if self_stat:
+				var self_effect := StatusEffect.new()
+				var status_to_apply := self_stat.duplicate()
+				self_effect.status = status_to_apply
+				self_effect.execute([battle_unit_owner])
+	else: return
